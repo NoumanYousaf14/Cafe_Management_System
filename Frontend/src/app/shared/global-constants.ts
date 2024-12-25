5b3fd0c5f340ea3a27a9e7aee6c414f8fa8dd046
hassansaleem694 <chhassan1041@gmail.com>
2024-12-25 16:01:24 +0500

    Validation change & Analysis

--- Frontend/src/app/shared/global-constants.ts
+++ Frontend/src/app/shared/global-constants.ts
@@ -16,7 +16,8 @@ export class GlobalConstant{
 
     public static emailRegex:string="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
 
-    public static contactNumberRegex:string="^[e0-9]{10,10}$";
+    public static contactNumberRegex: string = "^[0-9]{11}$";
+
 
 
     // Variable
